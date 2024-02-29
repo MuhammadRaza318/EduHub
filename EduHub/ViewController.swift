9b418ea2d5980f73a20bbb77b0e6eb4ec7acef59
Raza <mraza71318@gmail.com>
2024-03-01 01:34:23 +0500

    Initial Commit

---
+++ EduHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EduHub
+//
+//  Created by Raza on 01/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
